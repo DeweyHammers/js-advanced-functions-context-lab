2b83dabcf7a636e69df8329a173e778f5ea0077c
Dewey <hammersdrichard@gmail.com>
2021-07-02 07:37:31 -0700

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,64 @@
 /* Your Code Here */
 
+const createEmployeeRecord = (employee) => {
+    return {
+      firstName: employee[0],
+      familyName: employee[1],
+      title: employee[2],
+      payPerHour: employee[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    }
+  }
+
+const createEmployeeRecords = (records) => {
+    return records.map(record => createEmployeeRecord(record));
+}
+
+let createTimeInEvent = function(timeIn){
+    this.timeInEvents.push({
+        type: 'TimeIn',
+        date: timeIn.split(' ')[0],
+        hour: parseInt(timeIn.split(' ')[1])
+      });
+    return this
+}
+
+const createTimeOutEvent = function(timeOut){
+    this.timeOutEvents.push({
+      type: 'TimeOut',
+      date: timeOut.split(' ')[0],
+      hour: parseInt(timeOut.split(' ')[1])
+    })
+    return this
+  }
+
+const hoursWorkedOnDate = function(date){
+    let timeIn = this.timeInEvents.find(record => record.date === date);
+    const timeOut = this.timeOutEvents.find(record => record.date === date);
+    timeIn.hour === 1000 ? timeIn = 10 : timeIn = parseInt(timeIn.hour.toString().split('0'))
+    const hours = Math.abs(( timeIn - parseInt(timeOut.hour.toString().split('0')))) 
+    return hours
+}
+
+const wagesEarnedOnDate = function(date){
+    const hours = hoursWorkedOnDate.call(this, date)
+    return hours * this.payPerHour
+}
+
+const calculatePayroll = (employees) => {
+    const payroll = employees.map(employee => allWagesFor.call(employee));
+    return payroll.reduce((wage, total) => wage + total);
+}
+
+const findEmployeeByFirstName = (records, name) => {
+    return records.find(record => record.firstName === name);
+}
+  
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
+
  sneaky bug that we'll cover in the next few lessons!
 
  As a result, the lessons for this function will pass *and* it will be available
@@ -18,5 +74,6 @@ let allWagesFor = function () {
         return memo + wagesEarnedOnDate.call(this, d)
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
+ 
     return payable
 }
